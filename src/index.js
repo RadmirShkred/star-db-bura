71f911fc57ba05c9f7b319e9a5c096f6a39a22cf
radmir-shkred <psitecton@gmail.com>
2020-06-01 12:22:12 +0300

    commit 3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,46 @@
-const getResource = async (url) => {
-    const res = await fetch(url);
-    return await res.json();
-};
+class SwapiService {
 
-getResource('https://swapi.dev/api/people/1/')
-    .then(body => console.log(body));
+    _apiBase = 'https://swapi.dev/api';
+
+    async getResource(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${this._apiBase}${url}, reseived ${res.status}`);
+        }
+        return await res.json();
+    };
+
+    async getAllPeople() {
+        const res = await this.getResource(`/people/`);
+        return res.results;
+    }
+
+    getPerson(id) {
+        return this.getResource(`/people/${id}/`);
+    }
+
+    async getAllPlanets() {
+        const res = await this.getResource(`/planets/`);
+        return res.results;
+    }
+
+    getPlanet(id) {
+        return this.getResource(`/planets/${id}`);
+    }
+
+    async getAllStarships() {
+        const res = await this.getResource(`/starships/`);
+        return res.results;
+    }
+
+    getStarship(id) {
+        return this.getResource(`/starships/${id}`);
+    }
+}
+
+const swapi = new SwapiService();
+
+swapi.getStarship(3)
+    .then(starship => console.log(starship.name));
+
+
