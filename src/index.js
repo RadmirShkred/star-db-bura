7c5fb17d29865525a456196cd9c27174b83655c7
radmir-shkred <psitecton@gmail.com>
2020-06-01 10:50:01 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const getResource = async (url) => {
+    const res = await fetch(url);
+    return await res.json();
+};
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+getResource('https://swapi.dev/api/people/1/')
+    .then(body => console.log(body));
