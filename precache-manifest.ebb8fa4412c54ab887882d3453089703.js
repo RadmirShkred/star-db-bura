6917e8326ea3fdf81d67c9cd53a35bef8b11882d
radmir-shkred <psitecon@gmail.com>
2020-07-13 17:43:01 +0300

    Updates

---
+++ precache-manifest.ebb8fa4412c54ab887882d3453089703.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15c7d3b4982bef11231ac4fad54cf8c",
+    "url": "/star-db-bura/index.html"
+  },
+  {
+    "revision": "f98bc944d77056af8dc7",
+    "url": "/star-db-bura/static/css/main.e5aea675.chunk.css"
+  },
+  {
+    "revision": "96ad1a62272fe6ee3841",
+    "url": "/star-db-bura/static/js/2.3ce595d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-db-bura/static/js/2.3ce595d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f98bc944d77056af8dc7",
+    "url": "/star-db-bura/static/js/main.eb56bb39.chunk.js"
+  },
+  {
+    "revision": "77de7f332b1b322df6ad",
+    "url": "/star-db-bura/static/js/runtime-main.c4f23f93.js"
+  }
+]);
